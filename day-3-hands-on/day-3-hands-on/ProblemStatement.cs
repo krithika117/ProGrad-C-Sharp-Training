3a7245fd2b905be3ac381ea08a4b6e37cfa7234b
krithika117 <krithikanithyanandam1@gmail.com>
2023-03-23 16:26:03 +0530

    Solved problem statement

---
+++ day-3-hands-on/day-3-hands-on/ProblemStatement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_3_hands_on
+{
+    internal class ProblemStatement
+    {
+        public static void Main(string[] args)
+        {
+            int totalBullets = 14;
+            int speedDifference = 3;
+
+            int rallies = (int) totalBullets / (speedDifference + 1);
+            int remainder = totalBullets % (speedDifference + 1);
+            int heroRemainder = (remainder == 0) ? 0 : 1;
+            int villainRemainder = (remainder == 0) ? 0 : remainder-1;
+
+            for (int i = 1, j = 0; i <= rallies; i++, j+=speedDifference) {
+                Console.WriteLine("Hero has fired " + i + " shots");
+                Console.WriteLine("Villain has fired " + (j+speedDifference) + " shots");
+            }
+
+            Console.WriteLine("\nRemaining Bullets = " + remainder);
+            Console.WriteLine("Hero's Remaining Bullets = " + heroRemainder);
+            Console.WriteLine("Villain's Remaining Bullets = " + villainRemainder);
+
+            Console.WriteLine("\nWinner is the " + ((heroRemainder > villainRemainder) ? "Hero" : "Villain"));
+            
+        }
+
+    }
+}
