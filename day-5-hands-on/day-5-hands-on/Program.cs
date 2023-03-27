49cda21511c8bfe3e2ba2a5bf23d46566c3263df
krithika117 <krithikanithyanandam1@gmail.com>
2023-03-27 09:31:58 +0530

    Added Day-5 Folder

---
+++ day-5-hands-on/day-5-hands-on/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace day_5_hands_on
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
