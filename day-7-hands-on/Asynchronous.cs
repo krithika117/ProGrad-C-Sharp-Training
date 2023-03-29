9157d65d6ed5594909b1d50c2eb5a2fa97d46b56
krithika117 <krithikanithyanandam1@gmail.com>
2023-03-29 13:58:21 +0530

    Morning sesssion

--- day-7-hands-on/Asynchronous.cs
+++ day-7-hands-on/Asynchronous.cs
@@ -1,21 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace OOPS
 {
-    internal class Asynchronous
+    internal class newClass
     {
-        public static async void method1()
+
+        public async Task<string> Method1()
+        {
+            Console.WriteLine("Method 1 started");
+            await Task.Delay(6000);
+            Console.WriteLine("Method 1 ended");
+            return "";
+        }
+
+        public async Task<string> Method2()
+        {
+            Console.WriteLine("Method 2 started");
+            await Task.Delay(4000);
+            Console.WriteLine("Method 2 ended");
+            return "";
+        }
+        public async Task<string> Method3()
+        {
+            Console.WriteLine("Method 3 started");
+            await Task.Delay(2000);
+            Console.WriteLine("Method 3 ended");
+            return "";
+        }
+    }
+    internal class AsyncProgramming
+    {
+        static async Task Main(string[] args)
         {
 
-            Console.WriteLine("M1Start");
-            await Task.Delay(3000);
-            Console.WriteLine("M1End");
+            newClass obj = new newClass();
+            Console.WriteLine("Without await");
+            obj.Method1();
+            obj.Method2();
+            obj.Method3();
+            Console.ReadLine();
+            Console.WriteLine("With await");
+
+            await obj.Method1();
+            await obj.Method2();
+            await obj.Method3();
+            Console.ReadLine();
+
         }
-        public static async void method2()
-        {
-            Console.WriteLine("M2Start");
-            await Task.Delay(3000);
-            Console.WriteLine("M2End");
-        }
+
     }
 }
--- day-7-hands-on/Program.cs
+++ day-7-hands-on/Program.cs
@@ -1,27 +1,27 @@
-﻿using System;
+﻿// using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // List <int> ints = new List<int>() { 1,2,3,4,5,6,7 };
+// namespace MyApp // Note: actual namespace depends on the project name.
+// {
+//     internal class Program
+//     {
+//         static async Task Main(string[] args)
+//         {
+//             // List <int> ints = new List<int>() { 1,2,3,4,5,6,7 };
 
-            // Func<List<int>, int> maxFinder = (ints) => ints.Max();
-            // Console.WriteLine("Maximum = " + maxFinder(ints));
-            Asynchronous.method1();
+//             // Func<List<int>, int> maxFinder = (ints) => ints.Max();
+//             // Console.WriteLine("Maximum = " + maxFinder(ints));
+//             Asynchronous.method1();
+//             Console.ReadLine();
 
-            Asynchronous.method2();
-            Console.ReadLine();
-            // int n = 5;
-            // Func<int, int> factorial = null;
 
-            // factorial = (x) => x == 0 ? 1 : x * factorial(x - 1); 
+//             // int n = 5;
+//             // Func<int, int> factorial = null;
 
-            // int result = factorial(n); 
-            // Console.WriteLine($"The factorial of {n} is {result}");
+//             // factorial = (x) => x == 0 ? 1 : x * factorial(x - 1); 
 
-        }
-    }
-}
+//             // int result = factorial(n); 
+//             // Console.WriteLine($"The factorial of {n} is {result}");
+
+//         }
+//     }
+// }
