8c79a8af1fa3dae1dd51fb1808f14ba9cf29e459
krithika117 <krithikanithyanandam1@gmail.com>
2023-03-29 16:04:45 +0530

    file handling with async

--- day-7-hands-on/Asynchronous.cs
+++ day-7-hands-on/Asynchronous.cs
@@ -1,56 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace OOPS
-{
-    internal class newClass
-    {
+// namespace OOPS
+// {
+//     internal class newClass
+//     {
 
-        public async Task<string> Method1()
-        {
-            Console.WriteLine("Method 1 started");
-            await Task.Delay(6000);
-            Console.WriteLine("Method 1 ended");
-            return "";
-        }
+//         public async Task<string> Method1()
+//         {
+//             Console.WriteLine("Method 1 started");
+//             await Task.Delay(6000);
+//             Console.WriteLine("Method 1 ended");
+//             return "";
+//         }
 
-        public async Task<string> Method2()
-        {
-            Console.WriteLine("Method 2 started");
-            await Task.Delay(4000);
-            Console.WriteLine("Method 2 ended");
-            return "";
-        }
-        public async Task<string> Method3()
-        {
-            Console.WriteLine("Method 3 started");
-            await Task.Delay(2000);
-            Console.WriteLine("Method 3 ended");
-            return "";
-        }
-    }
-    internal class AsyncProgramming
-    {
-        static async Task Main(string[] args)
-        {
+//         public async Task<string> Method2()
+//         {
+//             Console.WriteLine("Method 2 started");
+//             await Task.Delay(4000);
+//             Console.WriteLine("Method 2 ended");
+//             return "";
+//         }
+//         public async Task<string> Method3()
+//         {
+//             Console.WriteLine("Method 3 started");
+//             await Task.Delay(2000);
+//             Console.WriteLine("Method 3 ended");
+//             return "";
+//         }
+//     }
 
-            newClass obj = new newClass();
-            Console.WriteLine("Without await");
-            obj.Method1();
-            obj.Method2();
-            obj.Method3();
-            Console.ReadLine();
-            Console.WriteLine("With await");
+//     internal class AsyncProgramming
+//     {
+//         static async Task Main(string[] args)
+//         {
+//             newClass obj = new newClass();
+//             Console.WriteLine("Without await");
+//             obj.Method1();
+//             obj.Method2();
+//             obj.Method3();
+//             Console.ReadLine();
 
-            await obj.Method1();
-            await obj.Method2();
-            await obj.Method3();
-            Console.ReadLine();
+//             Console.WriteLine("With await");
+//             await obj.Method1();
+//             await obj.Method2();
+//             await obj.Method3();
+//             Console.ReadLine();
 
-        }
-
-    }
-}
+//         }
+//     }
+// }
---
+++ day-7-hands-on/RealTimeAsync.cs
@@ -0,0 +1,42 @@
+// using System;
+// class Program
+// {
+//     FileStream fs1 = null;
+//     StreamWriter writer = null;
+//     StreamReader reader = null;
+//     async Task<String> fileOpenerMethod()
+//     {
+//         fs1 = new FileStream("C:\\Users\\HP\\source\\repos\\day-5-hands-on\\files-created\\textwriter.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+//         return "";
+//     }
+//     async Task<string> fileWriterMethod()
+//     {
+//         await fileOpenerMethod();
+//         await Task.Delay(2000);
+//         writer = new StreamWriter(fs1);
+//         writer.WriteLine("StreamWriter Demo");
+//         Console.WriteLine("Written");
+//         return "";
+//     }
+//     async Task<string> fileReaderMethod()
+//     {
+//         await fileWriterMethod();
+//         await Task.Delay(5000);
+
+//         reader = new StreamReader(fs1);
+//         string c = reader.ReadToEnd();
+//         Console.WriteLine(c);
+//         writer.Close();
+//         reader.Close();
+//         return "";
+//     }
+
+//     static async Task Main(string[] args)
+//     {
+
+//         Program p = new Program();
+//         await p.fileReaderMethod();
+//         p.fs1.Close();
+//     }
+
+// }
