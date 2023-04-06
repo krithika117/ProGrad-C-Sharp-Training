c18ed0353b6c7cb500ea65c9249ac19033b0b0d1
krithika117 <krithikanithyanandam1@gmail.com>
2023-04-06 14:36:57 +0530

    connection with db established

---
+++ SQL_Test_App/SQL_Test_App/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+namespace SQL_Test_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=DESKTOP-IRQ8TH4; Integrated Security=True; Encrypt=False; Initial Catalog=TestDB";
+            SqlConnection connection = new SqlConnection(connectionString);
+            
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM employee", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["emp_id"] + " "+ reader["emp_name"]);
+                }
+                reader.Close();
+                connection.Close();
+                Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
